5cb69604bc763c02959431750bc3c6c623592113
yuliia-h <yuliiagusak@gmail.com>
2020-01-16 14:17:37 +0200

    HW_Classes_Start

---
+++ 02_001_Classes/Classes/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_001_Classes
+{
+    class Point
+    {
+        int x, y;
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(int X, int Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+
+        public void PointShow(int x, int y)
+        {
+            
+        }
+    }
+}
