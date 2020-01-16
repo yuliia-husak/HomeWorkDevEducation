9fda34e4638cc8e6da9d61d5564912e01fb90125
yuliiagusak <yuliiagusak@gmail.com>
2020-01-16 21:46:58 +0200

    ArrayTwo

--- 02_001_Classes/Classes/ArrayTwo.cs
+++ 02_001_Classes/Classes/ArrayTwo.cs
@@ -8,5 +8,85 @@ namespace _02_001_Classes
 {
     class ArrayTwo
     {
+        //Поля:
+        private double[,] DoubleArray;
+        private int n;
+        private int m;
+
+        //Конструктор, позволяющий создать массив размерности n×m.
+        public ArrayTwo(int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+            DoubleArray = new double[n,m];
+        }
+
+        //Методы, позволяющие:
+        //ввести элементы массива с клавиатуры;
+        public void KeyboardArray()
+        {
+            for(int i = 0; i < DoubleArray.GetLength(0); i++, Console.WriteLine())
+            {
+                for(int j = 0; j < DoubleArray.GetLength(1); j++)
+                {
+                    Console.Write("A[{0}][{1}] = ", i, j);
+                    DoubleArray[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+        }
+        
+        //вывести элементы массива на экран;
+        public void Show()
+        {
+            Console.WriteLine("Вывод массива: ");
+            for (int i = 0; i < DoubleArray.GetLength(0); i++, Console.WriteLine())
+            {
+                for (int j = 0; j < DoubleArray.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", DoubleArray[i,j]);                    
+                }
+            }
+        }
+        //отсортировать элементы каждой строки массива в порядке убывания.
+        public void SortArray()
+        {
+            double temp;
+            for (int i = 0; i < DoubleArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < DoubleArray.GetLength(0) - 1; j++)
+                {
+                    for (int z = j + 1; z < DoubleArray.GetLength(1); z++)
+                    {
+                        if (DoubleArray[i, j] < DoubleArray[i, z])
+                        {
+                            temp = DoubleArray[i, j];
+                            DoubleArray[i, j] = DoubleArray[i, z];
+                            DoubleArray[i, z] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Свойства:
+        //возвращает общее количество элементов в массиве (доступное только для чтения);
+        public string Size => $"Размерность массива составляеть {DoubleArray.Length}";
+
+        //позволяющее увеличить значение всех элементов массива на скаляр(доступное только для записи).
+        public double Scalyar
+        {
+            set
+            {
+                for (int i = 0; i < DoubleArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < DoubleArray.GetLength(1); j++)
+                    {
+                        DoubleArray[i, j] *= value;
+                    }
+                }
+            }
+        }
+
+
     }
 }
--- 02_001_Classes/Program.cs
+++ 02_001_Classes/Program.cs
@@ -11,46 +11,56 @@ namespace _02_001_Classes
         static void Main(string[] args)
         {
             #region Point
-            Point p = new Point();
-            Point p1 = new Point("a", 2, 5);
+            //Point p = new Point();
+            //Point p1 = new Point("a", 2, 5);
 
-            p.X = 2;
-            p1.Y = 3;
+            //p.X = 2;
+            //p1.Y = 3;
 
-            p.ShowPoint();
-            p1.ShowPoint();
+            //p.ShowPoint();
+            //p1.ShowPoint();
 
-            p1.Name = "B";
-            Console.WriteLine(p1.Name);
+            //p1.Name = "B";
+            //Console.WriteLine(p1.Name);
 
            
-            p1.ScalyarX = 2;
-            p1.ScalyarY = 3;
-            p1.ShowPoint();
+            //p1.ScalyarX = 2;
+            //p1.ScalyarY = 3;
+            //p1.ShowPoint();
 
-            Console.Write("Enter vector coordinates: x = ");
-            int c = int.Parse(Console.ReadLine());
-            Console.Write("y = ");
-            int d = int.Parse(Console.ReadLine());
+            //Console.Write("Enter vector coordinates: x = ");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.Write("y = ");
+            //int d = int.Parse(Console.ReadLine());
 
-            p1.Vector(c, d);
-            p1.ShowPoint();
+            //p1.Vector(c, d);
+            //p1.ShowPoint();
 
-            p1.CalculateDistance(p1);
+            //p1.CalculateDistance(p1);
             #endregion
             #region ArrayOne
-            ArrayOne array = new ArrayOne(5);
-            array.KeyboardTouch();
-            array.Show();
-            array.SortArray();
-            array.Show();
-            Console.WriteLine(array.SizeArray);
-            array.Scalyar = 3;
-            array.Show();
+            //ArrayOne array = new ArrayOne(5);
+            //array.KeyboardTouch();
+            //array.Show();
+            //array.SortArray();
+            //array.Show();
+            //Console.WriteLine(array.SizeArray);
+            //array.Scalyar = 3;
+            //array.Show();
+            #endregion
+            #region
+            ArrayTwo two = new ArrayTwo(2, 2);
+            two.KeyboardArray();
+            two.Show();
+            Console.WriteLine(two.Size);
+            two.Scalyar = 2;
+            two.Show();
+            two.SortArray();
+            two.Show();
+
 
 
             #endregion
-
 
 
             Console.ReadKey();
