4b1ac0a60f5d9fded9bec7fa5c9c4f2caf8707d8
yuliia-h <yuliiagusak@gmail.com>
2020-01-16 15:45:06 +0200

    Point

--- 02_001_Classes/Classes/Point.cs
+++ 02_001_Classes/Classes/Point.cs
@@ -8,23 +8,59 @@ namespace _02_001_Classes
 {
     class Point
     {
-        int x, y;
+        private string name;
+        private int x;
+        private int y;
 
-        public Point()
+        public string Name { get { return name; } set { name = value; } }    
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+
+        public int ScalyarX
         {
-            x = 0;
-            y = 0;
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentOutOfRangeException(
+                        $"{nameof(value)} ноль не может быть.");
+                x = value * x;
+            }
         }
 
-        public Point(int X, int Y)
+        public int ScalyarY
         {
-            this.x = X;
-            this.y = Y;
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentOutOfRangeException(
+                        $"{nameof(value)} ноль не может быть.");
+                y = value * y;
+            }
         }
 
-        public void PointShow(int x, int y)
+        public Point() { name = "A"; x = 0; y = 0;}
+
+        public Point(string name, int x, int y)
         {
-            
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }                     
+        public void ShowPoint()
+        {
+            Console.WriteLine("Point {0}: x = {1}, y = {2} ", name, x, y);
+        }
+
+        public void Vector(int a, int b)
+        {
+            x += a;
+            y += b;
+        }
+
+        public void CalculateDistance(Point point)
+        {
+            double r = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            Console.WriteLine("Distance from point 0,0 to point {0} = {1:0.00} ", name, r);
         }
     }
 }
--- 02_001_Classes/Program.cs
+++ 02_001_Classes/Program.cs
@@ -10,6 +10,38 @@ namespace _02_001_Classes
     {
         static void Main(string[] args)
         {
+            #region Point
+            Point p = new Point();
+            Point p1 = new Point("a", 2, 5);
+
+            p.X = 2;
+            p1.Y = 3;
+
+            p.ShowPoint();
+            p1.ShowPoint();
+
+            p1.Name = "B";
+            Console.WriteLine(p1.Name);
+
+           
+            p1.ScalyarX = 2;
+            p1.ScalyarY = 3;
+            p1.ShowPoint();
+
+            Console.Write("Enter vector coordinates: x = ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int d = int.Parse(Console.ReadLine());
+
+            p1.Vector(c, d);
+            p1.ShowPoint();
+
+            p1.CalculateDistance(p1);
+            #endregion 
+
+
+
+            Console.ReadKey();
         }
     }
 }
