c9d784ccb0627ae59fbd3838ddc37bbc43026cf0
yuliia-h <yuliiagusak@gmail.com>
2020-02-13 11:16:10 +0200

    Thread_Example

---
+++ MyThreadWalkthroughApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+public class ServerClass
+{
+
+    static int count = 0;
+    // The method that will be called when the thread is started.
+    /// <summary>
+    /// Метод, который будет вызываться при запуске потока.
+    /// </summary>
+    public void InstanceMethod()
+    {
+        Console.WriteLine(
+            "ServerClass.InstanceMethod is running on another thread.");
+
+        int data = count++;
+        // Pause for a moment to provide a delay to make
+        // threads more apparent.
+        Thread.Sleep(3000);
+        Console.WriteLine(
+            "The instance method called by the worker thread has ended. " + data);
+    }
+}
+
+public class Simple
+{
+    public static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            CreateThreads();
+        }
+    }
+    public static void CreateThreads()
+    {
+        ServerClass serverObject = new ServerClass();
+
+        Thread InstanceCaller = new Thread(new ThreadStart(serverObject.InstanceMethod));
+        // Start the thread.
+        InstanceCaller.Start();
+
+        Console.WriteLine("The Main() thread calls this after "
+            + "starting the new InstanceCaller thread.");
+
+    }
+}
---
+++ Thread_Recursion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Recursion
+{
+    class Program
+    {
+        static void ThreadRecursion(object counter)
+        {    
+           int n = (int)counter;
+           if (n <= 0) return;
+           Thread thread = new Thread(ThreadRecursion);
+           thread.Priority = ThreadPriority.Lowest;
+           Thread.Sleep(300);
+           thread.Name = "Thread" + n;
+           thread.Start(n - 1);
+           Console.WriteLine("ThreadRecursion 1: " + Thread.CurrentThread.GetHashCode());  
+           Console.WriteLine(new string('-', 20));
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadRecursion(10);
+            Thread.Sleep(800);
+            Console.WriteLine("Main thread()");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread() for {0}", i);
+            }
+            
+           
+            
+            Console.Read();
+        }
+    }
+}
