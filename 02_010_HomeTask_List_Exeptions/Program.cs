82a129c3235ced43e866d65706bcd0b33909c0f8
yuliia-h <yuliiagusak@gmail.com>
2020-02-14 06:59:08 +0200

    ExeptBegin

---
+++ 02_010_HomeTask_List_Exeptions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_010_HomeTask_List_Exeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
