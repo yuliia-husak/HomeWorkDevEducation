0b219c903f4c490d6626045dcbe943a3d16e6b8e
yuliia-h <yuliiagusak@gmail.com>
2020-03-10 21:45:50 +0200

    HW_TE

---
+++ TextEditor_WPF/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextEditor_WPF
+{
+    public abstract class Command
+    {
+        public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UnExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
