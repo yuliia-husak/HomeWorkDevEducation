51cf57ecbcb42e4fdca3f11ee428f33b4367257d
yuliia-h <yuliiagusak@gmail.com>
2020-03-25 14:08:05 +0200

    CW_25032020_ADO

---
+++ AdoNetLibrary/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetLibrary
+{
+    class Program
+    {
+        SqlConnection conn = null;
+
+        public Program()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=SSPI;";
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            pr.InsertQuery();
+            pr.ReadData();
+
+            Console.ReadKey();
+            
+        }
+
+        public void InsertQuery()
+        {
+            try
+            {
+                conn.Open();
+                string insertString = @"insert into Authors (FirstName, LastName) values ('Roger', 'Zelazny')";
+                string insert2String = @"insert into Authors (FirstName, LastName) values ('Andrew', 'Troelsen')";
+                string insert3String = @"insert into Authors (FirstName, LastName) values ('Gerbert', 'Shild')";
+                SqlCommand cmd = new SqlCommand(insertString, conn);
+                SqlCommand cmd2 = new SqlCommand(insert2String, conn);
+                SqlCommand cmd3 = new SqlCommand(insert3String, conn);
+                cmd.ExecuteNonQuery();
+                cmd2.ExecuteNonQuery();
+                cmd3.ExecuteNonQuery();
+                
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+         public void ReadData()
+        {
+            SqlDataReader rdr = null;
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select * from Authors", conn);
+                rdr = cmd.ExecuteReader();
+                int line = 0;
+
+                while (rdr.Read()) 
+                {
+                    if (line == 0)
+                    {
+                        for(int i = 0; i < rdr.FieldCount; i++)
+                        {
+                            Console.Write(rdr.GetName(i).ToString() + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                    line++;
+                    Console.WriteLine(rdr[1] + " " + rdr[2]);
+                }
+                Console.WriteLine("O///// " + line.ToString());
+            }
+            finally
+            {
+                if(rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                if(conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Userdb_ADO/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Userdb_ADO
+{
+    class Program
+    {
+        SqlConnection conn = null;
+
+        public Program()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=UsersDB;Integrated Security=SSPI;";
+
+        }
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            //pr.InsertQuery();
+            pr.ReadData();
+
+            Console.ReadKey();
+           
+        }
+
+        public void InsertQuery()
+        {
+            try
+            {
+                conn.Open();
+                string insertString = @"insert into Users (Name, Age) values ('Iaaac', '10') ('Alla', '18') ('Alex', '15') ('Inna', '13') ('Tim', '16')";
+               
+                SqlCommand cmd = new SqlCommand(@"insert into Users (Name, Age) values ('Tim', '16')", conn);
+                
+                cmd.ExecuteNonQuery();
+                
+                
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public void ReadData()
+        {
+            SqlDataReader rdr = null;
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select * from Users where Age > 15", conn);
+                rdr = cmd.ExecuteReader();
+                
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[1] + " " + rdr[2]);
+                }
+                //int line = 0;
+
+                //while (rdr.Read())
+                //{
+                //    if (line == 0)
+                //    {
+                //        for (int i = 0; i < rdr.FieldCount; i++)
+                //        {
+                //            Console.Write(rdr.GetName(i).ToString() + " ");
+                //        }
+                //    }
+                //    Console.WriteLine();
+                //    line++;
+                //    Console.WriteLine(rdr);
+                //}
+                //Console.WriteLine("----" + line.ToString());
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
