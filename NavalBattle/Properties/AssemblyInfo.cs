77478f9cee23c55ec533f4e523c6ade20f0a2ab1
yuliia-h <yuliiagusak@gmail.com>
2020-04-07 00:02:12 +0300

    JS_BattleShip

---
+++ NavalBattle/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NavalBattle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NavalBattle")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dda6e2ba-b965-4584-bcf7-7a73a5e37ad9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NavalBattle/script.js
@@ -0,0 +1,28 @@
+﻿var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var quess;/*попытки*/
+var hits = 0;/*попадания*/
+var quesses = 0;/*кол-во попыток*/
+var isSunk = false; /*корабль потоплен или нет*/
+
+while (isSunk == false) {
+    quess = prompt("Ready, aim, fire! (enter a number 0-6) :");
+    if (quess < 0 || quess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        quesses = quesses + 1;
+        if (quess == location1 || quess == location2 || quess == location3) {
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+
+        } 
+    }    
+}
+
+var stats = "You took " + quesses + " quesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (3 / quesses);
+alert(stats);
