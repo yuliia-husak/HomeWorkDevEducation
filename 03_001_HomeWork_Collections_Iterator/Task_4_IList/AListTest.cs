8c29209aedf2e199dbc67252176432e58748a769
yuliia-h <yuliiagusak@gmail.com>
2020-04-10 17:06:25 +0300

    AListTest

--- 03_001_HomeWork_Collections_Iterator/Task_4_IList/AListTest.cs
+++ 03_001_HomeWork_Collections_Iterator/Task_4_IList/AListTest.cs
@@ -10,25 +10,40 @@ namespace _03_001_HomeWork_Collections_Iterator
 {
     [TestFixture]
     class AListTest
-    {
-        object[] vs = new object[] { "one","two","three" };               
-
+    {        
         [Test]
-        public void Test()
+        public void TestCountLength()
         {
-           // Assert.AreEqual(24, reservoir1.Volume());
+            var test = new AList(); 
+            test.Add("one");
+            test.Add("two");
+            test.Add("three");
+            int i = test.Count;
+           Assert.AreEqual(3, i);
         }
 
         [Test]
-        public void TestArea24Returned8()
+        public void TestInsertNewCount()
         {
-            //Assert.AreEqual(8, reservoir1.Area());
+            var test = new AList();
+            test.Add("one");
+            test.Add("two");
+            test.Add("three");
+
+            test.Insert(2, "insertTest");
+            Assert.AreEqual(4,test.Count);
         }
 
         [Test]
-        public void TestTypeReturned0()
+        public void TestDelete()
         {
-            //Assert.AreEqual(0, Reservoir.CompareType(reservoir1, reservoir2));
+            var test = new AList();
+            test.Add("one");
+            test.Add("two");
+            test.Add("three");
+
+            test.Clear();
+            Assert.AreEqual(0, test.Count);
         }
 
     }
