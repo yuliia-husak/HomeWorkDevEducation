9968df3ce91ebc7fbaaf19954b4ae67aec5d46c6
yuliia-h <yuliiagusak@gmail.com>
2020-04-17 00:32:46 +0300

    CarStore_Bootstrap

--- CarStore.WebUI/App_Start/RouteConfig.cs
+++ CarStore.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace CarStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Car", action = "List" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Car", action = "List", id = UrlParameter.Optional }
