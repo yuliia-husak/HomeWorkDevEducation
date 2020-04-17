54bcb4659f340e2079d5e07f825544d5b4848c63
yuliia-h <yuliiagusak@gmail.com>
2020-04-17 11:04:58 +0300

    CarStore_Cart

---
+++ CarStore.Domain/Entities/Cart.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CarStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Car car, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(g => g.Car.CarId == car.CarId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Car = car,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Car car)
+        {
+            lineCollection.RemoveAll(l => l.Car.CarId == car.CarId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Car.Price * e.Quantity);
+
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Car Car { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ CarStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using CarStore.Domain.Entities;
+using CarStore.Domain.Abstract;
+using CarStore.WebUI.Models;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private ICarRepository repository;
+        public CartController(ICarRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int carId, string returnUrl)
+        {
+            Car car = repository.Cars
+                .FirstOrDefault(g => g.CarId == carId);
+
+            if (car != null)
+            {
+                GetCart().AddItem(car, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int carId, string returnUrl)
+        {
+            Car car = repository.Cars
+                .FirstOrDefault(g => g.CarId == carId);
+
+            if (car != null)
+            {
+                GetCart().RemoveLine(car);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
